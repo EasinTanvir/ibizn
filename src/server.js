977d10400f6ab2a6abdcdecdc7f1bb423323d03d
EasinTanvir <easintanvir102@gmail.com>
2024-06-15 00:45:59 +0600

    updat packagejson

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
-const config = require("./src/config");
-const app = require("./src/app");
+const config = require("./config");
+const app = require("./app");
 
 // getting-started.js
 
