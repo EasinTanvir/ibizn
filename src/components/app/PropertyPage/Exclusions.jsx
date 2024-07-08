22e0524b48b615f9ba6bc54f4ba184cadccbf992
EasinTanvir <easintanvir102@gmail.com>
2024-07-08 15:37:55 +0600

    updated

--- src/components/app/PropertyPage/Exclusions.jsx
+++ src/components/app/PropertyPage/Exclusions.jsx
@@ -9,48 +9,32 @@ function Exclusions({ propertyData }) {
       <div className="  customContainer py-5 flex flex-col md:flex-row  justify-between gap-8  md:gap-12   md:py-20 md:bg-white px-4 lg:px-0">
         <div className="md:w-1/2 md:h-full flex flex-col items-start">
           <div className="flex gap-1 items-center mb-3">
-            <AddCircleIcon
-              className=" -ml-1 sm:ml-0 text-primary"
-              sx={{
-                fontSize: "44px",
-                color: "#3a95ea",
-                stroke: "#ffffff",
-                strokeWidth: 0,
-              }}
-            />
+            <img className="w-10" src="/as14.svg" alt="" />
             <h1 className="md:text-[3vw] md:text-[#0080FF]  text-[#0080FF] text-3xl">
               Trip Inclusions
             </h1>
           </div>
 
-          <div className="space-y-8 ">
+          <div className="md:space-y-4  space-y-2">
             {propertyData?.inclusions?.map((item, i) => (
-              <p key={i} className="md:text-2xl font-light text-secondary ">
-                {item}
+              <p key={i} className="text-xl font-light text-secondary ">
+                <span className="opacity-[70%]">—</span> {item}
               </p>
             ))}
           </div>
         </div>
         <div className="md:w-1/2 md:h-full flex flex-col items-start ">
           <div className="flex it gap-1 items-center mb-3">
-            <RemoveCircleIcon
-              className="-ml-1 sm:ml-0 text-primary"
-              sx={{
-                fontSize: "44px",
-                color: "#3a95ea",
-                stroke: "#ffffff",
-                strokeWidth: 0,
-              }}
-            />
+            <img className="w-10" src="/as15.svg" alt="" />
             <h1 className="md:text-[3vw] md:text-[#0080FF]  text-[#0080FF] text-3xl">
               Trip Exclusions
             </h1>
           </div>
 
-          <div className="space-y-8 ">
+          <div className="md:space-y-4  space-y-2">
             {propertyData?.exclusions?.map((item, i) => (
-              <p key={i} className="md:text-2xl font-light text-secondary ">
-                {item}
+              <p key={i} className="text-xl font-light text-secondary ">
+                <span className="opacity-[70%]">—</span> {item}
               </p>
             ))}
           </div>
--- src/components/app/PropertyPage/LiveaboardDetails.jsx
+++ src/components/app/PropertyPage/LiveaboardDetails.jsx
@@ -5,7 +5,17 @@ function LiveaboardDetails({ propertyData, resort }) {
     <div className="flex flex-col gap-20 lg:flex-row bg-white pb-16   sm:mt-16 mt-10   customContainer  justify-center px-4 lg:px-0">
       <div className="flex  flex-col items-start justify-start gap-2 font-light w-full  text-[#2f2f30]">
         <h1 className="text-3xl sm:mt-14 text-[#0080FF] md:text-6xl md:font-light md:text-[#0080FF] md:mb-2 font-outfit">
-          {resort ? "llike Resort" : "llike Liveaboard"}
+          {resort ? (
+            <span>
+              {propertyData?.nameOfProperty || propertyData?.propertyName}{" "}
+              Resort
+            </span>
+          ) : (
+            <span>
+              {propertyData?.nameOfProperty || propertyData?.propertyName}{" "}
+              Liveaboard
+            </span>
+          )}
         </h1>
         <p className=" text-secondary">
           {" "}
